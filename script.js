02e6c29180d7b97876683d7f568c00dd8155c02e
Your Name <youremail@domain.com>
2023-08-08 07:50:54 +0200

    ES6 String

---
+++ script.js
@@ -0,0 +1,12 @@
+let divStage1 = document.getElementById('stage1')
+
+//ES6 - Template Literal
+//String by `Backtick`
+
+let name = 'Ehsan';
+let favColor = 'Blue';
+let msg = `  Name:  ${name}  - Color: \' ${favColor} \'`;
+
+divStage1.innerHTML = msg;
+
+
