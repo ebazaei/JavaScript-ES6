094b6b9073a1ab570873ef1d470297479b69e86f
Your Name <youremail@domain.com>
2023-08-08 19:01:06 +0200

    classic for

--- script.js
+++ script.js
@@ -1,4 +1,21 @@
-let divStage1 = document.getElementById('stage1')
+let divStage1 = document.getElementById('stage1');
+let divStage2 = document.getElementById('stage2');
+
+
+//ES6 - LOOP
+let loopArr = [1,2,3,9,5,'test',7];
+let loopObject = {a:1,b:2,c:3};
+
+
+
+var res = "";
+for(i=0;i<loopArr.length;i++){
+    res = res + "  " + loopArr[i];
+}
+
+divStage2.innerHTML = res;
+
+
 
 //ES6 - Template Literal
 //String by `Backtick`
@@ -8,5 +25,7 @@ let favColor = 'Blue';
 let msg = `  Name:  ${name}  - Color: \' ${favColor} \'`;
 
 divStage1.innerHTML = msg;
+
+
 
 
